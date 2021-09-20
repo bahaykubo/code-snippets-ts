5b4d37dca7a19c7d5a25b41a4580abc4b238b6d2
noelsev <noel.sev@gmail.com>
2021-09-20 18:55:39 +1200

    Compare nested object properties and arrays

--- test-mocha/test/compare-objects.spec.ts
+++ test-mocha/test/compare-objects.spec.ts
@@ -23,4 +23,62 @@ describe('Compare Test', function() {
     expect(arays.some(aray => aray.includes('ok')), `Did not find jo in ${arays}`).to.be.true;
   });
 
+  it('should match object with nested array', function () {
+    const subject = {
+      'chargeName': 'Taffy daily charge 163202485888300a63',
+      'chargeReference': 'Taffy daily charge 163202485888300a63 ref',
+      'chargeType': 'DAILY',
+      'chargeBasis': 'c/day',
+      'billingDescriptor': 'Taffy daily charge 163202485888300a63 desc',
+      'schedules': [
+        {
+          'validFrom': '2020-11-01T00:00:00.000Z',
+          'validTo': '2020-11-05T00:00:00.000Z',
+          'rates': [
+            {
+              'rate': 10
+            }
+          ]
+        }
+      ]
+    };
+
+    const result = {
+      'id': 'bea6731a-4ff8-4fb5-9239-48b1ac88ccd7',
+      'chargeType': 'DAILY',
+      'chargeBasis': 'c/day',
+      'chargeReference': 'Taffy daily charge 163202485888300a63 ref',
+      'chargeTags': [
+        'ENERGY'
+      ],
+      'chargeCalculation': 'REQUIRED',
+      'schedules': [
+        {
+          'validFrom': '2020-11-01T00:00:00.000Z',
+          'validTo': '2020-11-05T00:00:00.000Z',
+          'rates': [
+            {
+              'rateQualifiers': [],
+              'applicableTimePeriods': [],
+              'timeRanges': [],
+              'rate': 10,
+              'spotPercentage': null,
+              'summarisationGroup': null
+            }
+          ]
+        }
+      ],
+      'chargeName': 'Taffy daily charge 163202485888300a63',
+      'billingDescriptor': 'Taffy daily charge 163202485888300a63 desc',
+      'flowDirection': null,
+      'powerFactor': null,
+      'topPeriods': null,
+      'sumFunction': null,
+      'losses': null
+    };
+
+    expect(result.schedules[0].rates[0]).to.deep.include(subject.schedules[0].rates[0]);
+    expect(result.schedules[0].rates[0]).to.include(subject.schedules[0].rates[0]);
+  });
+
 });
